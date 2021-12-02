ae21c7af2c66639dd8e15e6121a96eaba14c19c2
kej <itnt2019kara@gmail.com>
2021-12-02 15:02:33 +0900

    1202 오후학습

--- CH16_InnerClass/src/local_InnerClass2/App.java
+++ CH16_InnerClass/src/local_InnerClass2/App.java
@@ -22,16 +22,25 @@ public class App {
 	private void run() {
 		// 클래스 안의 메소드에 클래스
 		
-		class Printer implements Runnable{
-			@Override
-			public void run() {
-				System.out.println(name); // 내부클래스는 상위클래스의 변수를 사용가능
-			}
-		}
+		
+		
+//		class Printer implements Runnable{
+//			@Override
+//			public void run() {
+//				System.out.println(name); // 내부클래스는 상위클래스의 변수를 사용가능
+//			}
+//		}
 		
 //		new Printer().print();
+		
+//		이런방법도 있고, 매개변수에 직접 넣는 방법도 있다!
+		
 		ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
-		service.scheduleAtFixedRate(new Printer(), 0L, 1L, TimeUnit.SECONDS);
+		service.scheduleAtFixedRate(new Runnable() {
+			public void run() {
+				System.out.println(name);
+			}
+		}, 0L, 1L, TimeUnit.SECONDS);
 	}
 
 }
---
+++ CH17_Swing3/src/gui/Toolbar.java
@@ -0,0 +1,53 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+
+//class RedButtonListener implements ActionListener {
+//	private MainPanel mainPanel;
+//	public RedButtonListener(MainPanel mainPanel) {
+//		this.mainPanel = mainPanel;
+//	}
+//	@Override
+//	public void actionPerformed(ActionEvent e) {
+//		mainPanel.setBackground(Color.RED);
+//	}
+//}
+//
+//class BlueButtonListener implements ActionListener {
+//	private MainPanel mainPanel;
+//	public BlueButtonListener(MainPanel mainPanel) {
+//		this.mainPanel = mainPanel;
+//	}
+//	@Override
+//	public void actionPerformed(ActionEvent e) {
+//		mainPanel.setBackground(Color.BLUE);
+//	}
+//	
+//}
+
+
+public class Toolbar extends JToolBar {
+	private static final long serialVersionUTD = 1L;
+	
+	public Toolbar(MainPanel mainPanel) {
+		final JButton redButton = new JButton("RED");
+		final JButton blueButton = new JButton("BLUE");
+		// 버튼에 이벤트를 연결('클릭' 이벤트)
+		redButton.addActionListener(e -> mainPanel.setBackground(Color.RED));
+		
+		blueButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mainPanel.setBackground(Color.BLUE);
+			}
+		});
+		
+		// 툴바에 버튼을붙임
+		add(redButton);
+		add(blueButton);
+	}
+}
